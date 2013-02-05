56b50adbe5cdfc87ed69f7bb9538508e06732311
Dean Moses <dmoses@salesforce.com>
2013-02-05 14:25:55 -0800

    body now has 'authenticated' class when user is logged in

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,21 @@
 define(['modules/fn'], function (fn) {
+
+	//
+	// DETERMINE WHETHER USER IS LOGGED IN
+	//
+	
+	gallery.readCookie = function(name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+			var c = ca[i];
+			while (c.charAt(0)==' ') c = c.substring(1,c.length);
+			if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
+	}
+	
+	gallery.isLoggedIn = function() { return gallery.readCookie('G2_hybrid') ? true : false; };
 	
 	//
 	// MODELS
@@ -286,5 +303,9 @@ define(['modules/fn'], function (fn) {
     
     Handlebars.registerHelper("addSome", function(num) {
 		return num + 2;
-	});
+	 });
+	 
+	 if (gallery.isLoggedIn()) {
+	 	$("body").addClass('authenticated');
+	 }
 });
